c1c32c60a70500d675344f8bbb2006a4e5360bd6
Sudaukecoder <nguyengiahu29@gmail.com>
2026-03-04 20:55:14 +0700

    Fix bug register5

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-console.log('API URL:', process.env.REACT_APP_API_URL);
+console.log('API URL:', import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
 });
